25057ab55973e91c3243865452b69ae174d89776
siddiquisd <165915444+siddiquisd@users.noreply.github.com>
2024-05-04 04:42:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+'#!/usr/bin/env node';
+import inquirer from "inquirer";
+let todo = ["Mohsin"];
+async function newFunction(todo) {
+    do {
+        let welcome = console.log("welcome to todo list app!");
+        let operation = await inquirer.prompt([
+            {
+                name: "operator",
+                message: "what you want to do ? ",
+                type: "list",
+                choices: ["Add", "View", "Update", "Delete"],
+            }
+        ]);
+        if (operation.operator === "Add") {
+            let add = await inquirer.prompt({
+                name: "additems",
+                message: "plz add items",
+                type: "input",
+            });
+            todo.push(add.additems);
+            console.log(todo);
+        }
+        if (operation.operator === "View") {
+            console.log(todo);
+        }
+        ;
+        if (operation.operator === "Update") {
+            let update = await inquirer.prompt({
+                name: "updateitems",
+                message: "select to update items",
+                type: "list",
+                choices: todo
+            });
+            let upDate = await inquirer.prompt({
+                name: "updateitems2",
+                message: " update items",
+                type: "input",
+            });
+            let newTodo = todo.filter(val => val != update.updateitems);
+            todo = [...newTodo, upDate.updateitems2];
+        }
+        ;
+        if (operation.operator === "Delete") {
+            let remove = await inquirer.prompt({
+                name: "deleteitems",
+                message: "select item to delete",
+                type: "list",
+                choices: todo
+            });
+        }
+        ;
+    } while (true);
+}
+;
+newFunction(todo);
